08d3c6cddca876b949847538d2efbdefc9978a90
hitendert <hitendert@gmail.com>
2018-05-29 21:53:48 +0530

    First commit, layout is done, no code yet

---
+++ HaveToBudget/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HaveToBudget
+//
+//  Created by Hitender Thejaswi on 5/28/18.
+//  Copyright © 2018 Hitender Thejaswi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
